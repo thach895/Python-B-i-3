8d7c2b1f1e52a644cb405be6d5cfdad528d8711f
thach895 <ngo67775@gmail.com>
2026-05-25 16:21:30 +0700

    Add files via upload

---
+++ Bài 3.py
@@ -0,0 +1,16 @@
+print("===== HỆ THỐNG TIẾP NHẬN BỆNH NHÂN =====")
+
+# Nhập thông tin bệnh nhân
+name_patient = input("Nhập họ tên bệnh nhân: ")
+medical_id = input("Nhập mã bệnh án: ")
+department = input("Nhập khoa/phòng khám: ")
+
+# Hiển thị phiếu khám bệnh
+print("\n===== PHIẾU KHÁM BỆNH ĐIỆN TỬ =====")
+print("Tên bệnh nhân :", name_patient)
+print("Mã bệnh án    :", medical_id)
+print("Khoa khám     :", department)
+print("===================================")
+
+# Thông báo hoàn tất
+print("Tiếp nhận bệnh nhân thành công!")
